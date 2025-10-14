b6ffc237ea62da905b375b07008bd93dcb6803f1
vinivmarinho <vini.marinho2004@gmail.com>
2025-10-14 08:51:12 -0400

    feat: Adiciona função para adicionar pedidos na lista de pedidos
    E também corrige o problema da função de fábrica (não usava o return)

--- funcoes_fabrica_e_padrao_modulo/ex007.js
+++ funcoes_fabrica_e_padrao_modulo/ex007.js
@@ -1,19 +1,39 @@
 const restaurante = (function() {
+    let listaPedidos = []
+
     function criaPedidos(item, qtd, status = "Em andamento") {
-        this.item = item
-        this.qtd = qtd
-        this.status = status
-        this.mudaStatusPedido = function() {
+        mudaStatusPedido = function() {
             if (this.status === "Em andamento") {
                 this.status = "Pedido finalizado"
             } else {
                 this.status = "Em andamento"
             }
         }
-        console.log(`Pedido: ${this.item}, Qtd: ${this.qtd} Em andamento`)
+        return {item, qtd, status, mudaStatusPedido}
     }
+
+    function addPedido(item, qtd, status = "Em andamento") {
+        let pedido = criaPedidos(item, qtd, status)
+        listaPedidos.push(pedido)
+        console.log(`${item} (qtd: ${qtd}) adicionado à lista de pedidos `)
+    }
+
+    function alteraStatusPedido(nomePedido) {
+        const pedido = listaPedidos.find(p => p.item === nomePedido) // Percorre a lista e encontra o pedido que possui o mesmo nome do parâmetro  
+        if (pedido) {
+        pedido.mudaStatusPedido()
+        console.log(`Status do pedido: ${pedido.status}`)
+        console.log
+        } else {
+            console.log("Pedido não foi encontrado")
+            
+        }
+    }
+
     return {
-        criaPedidos
+        addPedido,
+        alteraStatusPedido
     }
 })();
-const teste = restaurante.criaPedidos("Pizza", "12")
+const teste = restaurante.addPedido("Sushi", "120") // Recebe o objeto do pedido recém criado
+restaurante.alteraStatusPedido("Sushi")
