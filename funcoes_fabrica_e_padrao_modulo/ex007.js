9a3eec971b40105e2c37b6688fa503177d073159
vinivmarinho <vini.marinho2004@gmail.com>
2025-10-14 09:12:45 -0400

    feat: Adiciona função que mostra todos os pedidos

--- funcoes_fabrica_e_padrao_modulo/ex007.js
+++ funcoes_fabrica_e_padrao_modulo/ex007.js
@@ -1,6 +1,10 @@
+// Uso uma função IFEE (Função auto executável) com o objetivo de criar um escopo isolado, não permitindo que variáveis e funções iternas fiquem acessíveis fora
+// Dentro dela, usei o module Pattern que retorna um objeto com métodos públicos (addPedido, alteraStatusPedido), enquanto mantenho outros dados internos privados (listaPedidos, criaPedidos)
 const restaurante = (function() {
+    // Variável privada ao módulo, somente as funções (addPedido e alteraStatusPedido, mostrarPedidos) conseguem acessá-la. Garante o encapsulamento e segurança dos dados
     let listaPedidos = []
 
+    // Função de fábrica que cria e retorna meus objetos que representam pedidos. Cada objeto terá suas propriedades (item, qtd, status)
     function criaPedidos(item, qtd, status = "Em andamento") {
         mudaStatusPedido = function() {
             if (this.status === "Em andamento") {
@@ -12,6 +16,7 @@ const restaurante = (function() {
         return {item, qtd, status, mudaStatusPedido}
     }
 
+    // Cria o pedido usando a função de fábrica e adiciona ele na lista de pedidos
     function addPedido(item, qtd, status = "Em andamento") {
         let pedido = criaPedidos(item, qtd, status)
         listaPedidos.push(pedido)
@@ -23,17 +28,31 @@ const restaurante = (function() {
         if (pedido) {
         pedido.mudaStatusPedido()
         console.log(`Status do pedido: ${pedido.status}`)
-        console.log
         } else {
             console.log("Pedido não foi encontrado")
             
         }
     }
 
+    function mostrarPedidos() {
+        listaPedidos.forEach((pedido) => {
+            console.log(`Item: ${pedido.item}`)
+            console.log(`Quantidade:${pedido.qtd}`)
+            console.log(`Status ${pedido.status}`)
+            console.log("=".repeat(20))
+        })
+    }
+    // retornando todas as funções que o módulo permite acessar externamente
     return {
         addPedido,
-        alteraStatusPedido
+        alteraStatusPedido,
+        mostrarPedidos
     }
 })();
 const teste = restaurante.addPedido("Sushi", "120") // Recebe o objeto do pedido recém criado
-restaurante.alteraStatusPedido("Sushi")
+const teste2 = restaurante.addPedido("Pizza", "12")
+const teste3 = restaurante.addPedido("Cebola", "16")
+const teste4 = restaurante.addPedido("Macarrão", "5")
+const teste5 = restaurante.addPedido("Camarão", "75")
+const teste6 = restaurante.addPedido("Sushi", "120")
+restaurante.mostrarPedidos()
