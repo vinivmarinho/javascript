80f239f1caa9f02fa82c13c2ac3afbdaa0656c86
vinivmarinho <vini.marinho2004@gmail.com>
2025-10-16 10:45:10 -0400

    feat: adiciona função construtora e método de venda no protótipo

---
+++ funcoes_fabrica_e_padrao_modulo/ex010.js
@@ -0,0 +1,31 @@
+/* Nível 4 — Projeto completo
+🏪 7. Sistema de Loja (Projeto Final)
+Monte um módulo completo combinando tudo:
+Factory Function → cria produtos (nome, preço, estoque);
+Prototype → adiciona métodos como vender(qtd) e repor(qtd);
+Module Pattern + IIFE → controla a lista de produtos e mantém os dados privados;
+Métodos públicos: adicionarProduto, listarProdutos, venderProduto(nome, qtd).
+💡 Use forEach para exibir o estoque e find para localizar produtos pelo nome. */
+
+// Vou usar função construtora e prototype para fazer os produtos
+// Construtora de objeto
+function Produto(nome, preco, qtdEstoque) {
+    this.nome = nome
+    this.preco = preco
+    this.qtdEstoque = qtdEstoque
+    console.log(`Produto ${this.nome} criado`)
+    console.log()
+}
+// Método de vender (do protótipo)
+Produto.prototype.vender = function(qtdVendida) {
+    if (this.qtdEstoque >= qtdVendida) {
+        this.qtdEstoque -= qtdVendida
+        console.log(`Produto vendido: ${this.nome}`)
+        console.log(`Quantidade vendida: ${qtdVendida}`)
+        console.log(`Total da compra: R$${qtdVendida * this.preco} reais`)
+    } else {
+        console.log("Quantidade excede o que existe no estoque")
+    }
+}
+const p1 = new Produto("Lápis", 10.50, 10)
+p1.vender(9)
