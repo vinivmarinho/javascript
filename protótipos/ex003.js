e13a66ace4725cf8ba01c61b6aca141a3b558279
vinivmarinho <vini.marinho2004@gmail.com>
2025-10-27 21:12:40 -0400

    feat: Adiciona exercício de um sistema de contas bancárias usando protótipo e função construtora

--- protótipos/ex003.js
+++ protótipos/ex003.js
@@ -7,3 +7,40 @@ verSaldo()
 Depois:
 Crie dois clientes diferentes e teste depósitos e saques.
 💰 Cenário real de controle financeiro. */
+
+// Função construtora:
+function Conta(cliente, saldo) {
+    this.cliente = cliente,
+    this.saldo = saldo
+    console.log(`Conta do(a) cliente ${this.cliente} criada. Saldo atual => R$${this.saldo.toFixed(2)} reais`)
+}
+// Adiciona método ao prototype
+Conta.prototype.depositar = function(valorDeposito) {
+    if (valorDeposito > 0) {
+        this.saldo += valorDeposito
+        console.log(`R$${valorDeposito} reais foram depositados na conta do(a) ${this.cliente}. Saldo atual => ${this.saldo.toFixed(2)}`)
+    } else {
+        console.log("Valor precisa ser maior que zero para o depósito")
+    }
+}
+
+Conta.prototype.sacar = function(valorSaque) {
+    if (valorSaque > 0) {
+        if (valorSaque <= this.saldo) {
+            this.saldo -= valorSaque
+            console.log(`O(a) cliente ${this.cliente} sacou R$${valorSaque} reais. Saldo atual => ${this.saldo.toFixed(2)} reais`)
+        } else {
+            console.log("Saldo insuficiente")
+        }
+    } else {
+        console.log("Valor para saque precisa ser maior que 0")
+    }
+}
+Conta.prototype.verSaldo = function() {
+    console.log(`O(a) cliente ${this.cliente} possui R$${this.saldo} reais em sua conta`)
+}
+
+let conta = new Conta("Vini", 2000)
+conta.depositar(1000)
+conta.sacar(2000)
+conta.verSaldo()
---
+++ protótipos/ex004.js
@@ -0,0 +1,9 @@
+/* Desafio Final: Sistema de Vendas
+Monte um pequeno sistema usando várias funções construtoras com protótipos:
+Produto(nome, preco)
+Cliente(nome, tipo)
+Venda(cliente, produto, quantidade)
+No protótipo de Venda, adicione:
+calcularTotal() → aplica desconto se o cliente for VIP
+resumo() → mostra um relatório da compra no console
+🎯 Esse é um exercício muito próximo de algo que você pode ver em um sistema real de gestão de vendas */
